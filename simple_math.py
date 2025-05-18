813648d391b9ffe7a9266b48716d7249ea42ec92
ICH <yev.guta@gmail.com>
2025-05-18 16:57:08 +0200

    add py-file simple_math.py

---
+++ simple_math.py
@@ -0,0 +1,8 @@
+class SimpleMath:
+    """ Класс с простыми математическими операциями."""
+    def square(self,x):
+        """Возвращает квадрат числа."""
+        return x * x
+    def cube(self,x):
+        """Возвращает куб числа"""
+        return x * x * x
