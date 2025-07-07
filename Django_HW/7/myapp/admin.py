3fd0ed224384636f20a36b5c2bf99dfbe443baf9
ICH <yev.guta@gmail.com>
2025-07-08 01:40:20 +0200

    update admin.py in django homework

--- Django_HW/7/myapp/admin.py
+++ Django_HW/7/myapp/admin.py
@@ -6,6 +6,11 @@ from .models import Category, Task, SubTask
 # admin.site.register(Task)
 # admin.site.register(SubTask)
 
+# Inline форма для подзадач внутри задачи
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1 # количество пустых форм по умолчанию
+
 @admin.register(Category)  # регистрирует класс с кастомными настройками
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('name',)  # Отображает категории в виде списка
@@ -13,10 +18,22 @@ class CategoryAdmin(admin.ModelAdmin):
 
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ('title', 'status', 'deadline', 'created_at') # определяет, какие поля будут видны в таблице админки
+    list_display = ('short_title', 'status', 'deadline', 'created_at') # определяет, какие поля будут видны в таблице админки
     list_filter = ('status', 'deadline') # добавляет фильтры по статусу и дедлайну, упрощая навигацию
     search_fields = ('title', 'description')  # позволяет искать задачи по title и description
     ordering = ['-created_at']  # сортирует список по убыванию даты создания.
+    inlines = [SubTaskInline]
+
+    # Укороченное отображение длинных названий
+    @admin.display(description='Заголовок')
+    def short_title(self, obj):
+        return obj.title if len(obj.title) <= 10 else obj.title[:10] + '...'
+
+# Action для массового изменения статуса подзадач
+@admin.action(description="Пометить как 'Done'")
+def mark_as_done(modeladmin, request, queryset):
+    updated = queryset.update(status='done')
+    modeladmin.message_user(request, f"{updated} подзадач помечено как 'Done'.")
 
 @admin.register(SubTask)
 class SubTaskAdmin(admin.ModelAdmin):
@@ -24,3 +41,4 @@ class SubTaskAdmin(admin.ModelAdmin):
     list_filter = ('status', 'deadline')
     search_fields = ('title', 'description')
     ordering = ['-created_at']
+    actions = [mark_as_done]
