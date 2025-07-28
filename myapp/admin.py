3248b5a5c10e646abb55f59c1a913f2c0765ac38
ICH <yev.guta@gmail.com>
2025-07-28 20:06:28 +0200

    update admin.py, admin.py (HW 11)

--- myapp/admin.py
+++ myapp/admin.py
@@ -8,6 +8,12 @@ class CategoryAdmin(admin.ModelAdmin):
     list_filter = ('name',)
     ordering = ('name',)
 
+class SubTaskInline(admin.TabularInline):
+    model = SubTask
+    extra = 1  # Количество пустых форм по умолчанию
+    fields = ('title', 'description', 'status', 'deadline')
+    show_change_link = True  # Ссылка для редактирования подзадачи
+
 @admin.register(Task)
 class TaskAdmin(admin.ModelAdmin):
     list_display = ('title', 'status', 'deadline', 'deadline_date', 'created_at', 'get_categories')
@@ -17,6 +23,13 @@ class TaskAdmin(admin.ModelAdmin):
     filter_horizontal = ('categories',)
     ordering = ('-created_at',)
     list_per_page = 25
+    inlines = [SubTaskInline]  # Добавляем инлайн-форму для подзадач
+
+    # укороченное отображение в списке подзадач
+    def short_title(self, obj):
+        return obj.title[:10] + '...' if len(obj.title) > 10 else obj.title
+
+    short_title.short_description = 'Title'
 
     def get_categories(self, obj):
         return ", ".join(category.name for category in obj.categories.all())
@@ -30,3 +43,12 @@ class SubTaskAdmin(admin.ModelAdmin):
     date_hierarchy = 'deadline'
     ordering = ('-created_at',)
     list_per_page = 25
+
+    # создание акшена
+    actions = ['make_done']
+
+    def make_done(self, request, queryset):
+        updated = queryset.update(status='DONE')
+        self.message_user(request, f"{updated} subtask(s) marked as Done.")
+
+    make_done.short_description = "Mark selected subtasks as Done"
